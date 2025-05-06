81b7eb19f01a17cb12fe451fda77227e57855af1
avmugesh <46862942+avmugesh@users.noreply.github.com>
2025-05-07 01:13:40 +0530

    Create main.tf

---
+++ lambda/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_function"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "MyLambdaFunction"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
